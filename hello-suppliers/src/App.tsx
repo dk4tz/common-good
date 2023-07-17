41755049aa9126bc6dfd31470ac4124e95dfa46c
David Katz <davidktz@Davids-MacBook-Air.local>
2023-07-17 13:34:39 -0400

    checkpoint

--- hello-suppliers/src/App.tsx
+++ hello-suppliers/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { useMutation } from 'react-query';
-import axios from 'axios';
+// import { useMutation } from 'react-query';
+// import axios from 'axios';
 
 type FileInputEvent = React.ChangeEvent<HTMLInputElement>;
 
@@ -31,6 +31,7 @@ function App() {
     setState((prev) => ({ ...prev, projectFiles: files }));
   };
 
+  /*
   const mutation = useMutation(async (data: AppState) => {
     const response = await axios.post('/api/projects', { name: data.projectName });
     const projectId = response.data.id;
@@ -49,6 +50,7 @@ function App() {
       setState((prev) => ({ ...prev, error: error.message }));
     }
   });
+  */
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -61,27 +63,27 @@ function App() {
       return;
     }
 
-    mutation.mutate(state);
+    // mutation.mutate(state);
   };
 
   return (
     <div className="p-4">
       <h1 className="text-4xl font-bold mb-4">Welcome to the Common Good Marketplace</h1>
-      <h2 className="text-2xl mb-4">We're excited you're here! Please answer the questions below to get started:</h2>
+      <h3 className="text-2xl mb-4">We're excited you're here! Please answer the following questions:</h3>
       <form className="space-y-4" onSubmit={handleSubmit}>
         <label className="block">
-          <span className="text-gray-700">What is your project's name?</span>
+          <span className="text-white-700">What is your project's name?</span>
           <input type="text" name="projectName" onChange={handleChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
         </label>
         <label className="block">
-          <span className="text-gray-700">Please upload any relevant documents that describe your impact (up to 5 files)</span>
+          <span className="text-white-700">Please upload any relevant documents that describe your impact (up to 5 files)</span>
           <input type="file" accept=".pdf" onChange={handleFileChange} multiple className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
         </label>
         <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Submit
         </button>
       </form>
-      {mutation.isError && <div className="text-red-500 mt-4">{state.error}</div>}
+      {state.error && <div className="text-red-500 mt-4">{state.error}</div>}
     </div>
   );
 }
